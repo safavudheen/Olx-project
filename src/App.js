764a91934c399e1287013db0cddfb7ad924b4b67
Safavudheen <safavudheen@gmail.com>
2021-07-01 08:04:02 -0700

    finished deployed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{useEffect,useContext} from 'react';
-import {BrowserRouter as Router ,Route} from 'react-router-dom'
+import { HashRouter as Router ,Route} from 'react-router-dom'
 import Signup from './Pages/Signup'
 import Login from './Pages/Login'
 import Create from './Pages/Create'
